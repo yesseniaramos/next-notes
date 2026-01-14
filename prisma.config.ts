ac7948d6bd9c3c863eb20545ccc3992348ebe3b4
Yessenia Ramos <yessenia.ramos.martinez@outlook.com>
2026-01-14 13:55:00 -0500

    Prisma version. and implementation updated.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,13 @@
 import 'dotenv/config'
 import type { PrismaConfig } from "prisma";
-import { env } from "prisma/config";
 
 export default {
-  schema: "src/db/schema",
+  schema: "src/db/schema/schema.prisma",
   migrations: {
     path: "src/db/migrations",
     seed: 'tsx src/db/seed.ts',
   },
   datasource: {
-    url: env("DATABASE_URL")
+    url: process.env.DATABASE_URL!
   }
 } satisfies PrismaConfig;
--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,9 +1,16 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+function createPrismaClient() {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+}
+
+export const prisma = globalForPrisma.prisma || createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
